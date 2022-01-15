2e97aeecc54638a480563e8f42e453f4ab9d7997
hrguan <hrguan@appannie.com>
2022-01-15 15:41:58 -0800

    update DB

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
   // Enter your own database information here based on what you created
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-adjacent-12420',
     user : 'hrguan',
     password : '',
     database : 'template1'
