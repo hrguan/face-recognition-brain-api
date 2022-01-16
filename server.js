efae8e3f9ed277a35a825e269bfc29ef8ba82d95
hrguan <hrguan@appannie.com>
2022-01-15 16:04:23 -0800

    update connection

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    rejectUnauthorized: false
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
